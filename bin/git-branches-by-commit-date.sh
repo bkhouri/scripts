76ffe29cccd650cc9198eda2845462fe88a24bc7
Bassam Khouri <bassam.khouri@cenx.com>
2017-10-19 12:26:01 -0400

    Add git-branch-by-commit-date.sh script

---
+++ bin/git-branches-by-commit-date.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# Reference https://gist.github.com/jasonrudolph/1810768
+for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci @ %cr " $branch | head -n 1` @ $branch; done | sort -r | column -t -s '@'
