5fb5a4a6fe2a40784a35bdf2df60605af8a70841
Bassam Khouri <bassam.khouri@cenx.com>
2017-10-27 09:57:07 -0400

    I'm used to IntelliJ where changes are automatically saved.  I had previously used Sublime Text, which required a manual 'save'... which I forgot to do, but did now

--- bin/git-branches-by-commit-date.sh
+++ bin/git-branches-by-commit-date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git fetch --pull
+git fetch --prune
 
 # Reference https://gist.github.com/jasonrudolph/1810768
 for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci @ %cr " $branch | head -n 1` @ $branch; done | sort -r | column -t -s '@'
